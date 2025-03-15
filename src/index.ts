bfc9b45371fe15fc54de2b228f08f93b20a6c621
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-03-15 11:50:30 +0700

    Lab07_Task1.8

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 import { uploadFile } from './services/uploadFileService';
 const app = express();
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://lab06-front-h3crh8djk-chalisa424s-projects.vercel.app'];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins
