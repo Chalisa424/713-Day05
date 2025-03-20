a1280d4b197345dbd8ecb540faa8766f053d9926
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-03-21 00:33:00 +0700

    Add_participantRoutes

---
+++ src/routes/participantRoute.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+import * as service from "../services/participantService";
+import type { Participant } from "../models/participant";
+
+const router = express.Router();
+
+// Get all participants
+router.get("/", async (req, res) => {
+    const pageSize = parseInt(req.query.pageSize as string) || 3;
+    const pageNo = parseInt(req.query.pageNo as string) || 1;
+    const keyword = req.query.keyword as string;
+
+    try {
+        const result = await service.getAllParticipantsWithPagination(keyword, pageSize, pageNo);
+        if (result.participants.length === 0) {
+            res.status(404).send("No participant found");
+            return;
+        }
+        res.setHeader("x-total-count", result.count.toString());
+        res.setHeader("Access-Control-Expose-Headers", "x-total-count");
+        res.json(result.participants);
+    } catch (error) {
+        console.error("Error occurred: ", error);
+        if (pageNo < 1 || pageSize < 1) {
+            res.status(400).send("Invalid pageNo or pageSize");
+        } else {
+            res.status(500).send("Internal Server Error");
+        }
+    }
+});
+
+export default router;
